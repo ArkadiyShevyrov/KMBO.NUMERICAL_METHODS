c805df1f31e2b19715392688d82070da6fbd6fac
a.shevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2024-09-29 02:37:19 +0300

    KMBO-NM. Front. T1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import {Nav, Navbar} from 'react-bootstrap';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import Task_1_1 from "./components/tasks/Task_1_1";
-import Task_1_6 from "./components/tasks/Task_1_6";
 import Task_1_5 from "./components/tasks/Task_1_5";
+import Task_1_6 from "./components/tasks/Task_1_6";
 
 const App: React.FC = () => {
     return (
         <Router>
-            <Navbar bg="light">
-                <Navbar.Brand href="/">Задания: </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="/task_1_1">
-                        Задача №1.1
-                    </Nav.Link>
-                    <Nav.Link href="/task_1_2">
-                        Задача №1.2
-                    </Nav.Link>
-                    <Nav.Link href="/task_1_3">
-                        Задача №1.3
-                    </Nav.Link>
-                    <Nav.Link href="/task_1_4">
-                        Задача №1.4
-                    </Nav.Link>
-                    <Nav.Link href="/task_1_5">
-                        Задача №1.5
-                    </Nav.Link>
-                    <Nav.Link href="/task_1_6">
-                        Задача №1.6
-                    </Nav.Link>
-                </Nav>
+            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                <Container fluid>
+                    <Navbar.Brand as={Link} to="/">Задания</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+                            {['1', '2', '3', '4'].map(part => (
+                                <NavDropdown key={part} title={`Задания №${part}.1-6`} id={`navbarScrollingDropdown${part}`}>
+                                    {Array.from({ length: 6 }, (_, index) => (
+                                        <NavDropdown.Item key={index} as={Link} to={`/task_${part}_${index + 1}`}>
+                                            Задача №{part}.{index + 1}
+                                        </NavDropdown.Item>
+                                    ))}
+                                </NavDropdown>
+                            ))}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
-            <Routes>
-                <Route path="/task_1_1" element={<Task_1_1/>}/>
-                <Route path="/task_1_5" element={<Task_1_5/>}/>
-                <Route path="/task_1_6" element={<Task_1_6/>}/>
-            </Routes>
+
+            <Container className="mt-4">
+                <Routes>
+                    <Route path="/" element={<div>Главная страница</div>} />
+                    <Route path="/task_1_1" element={<Task_1_1 />} />
+                    <Route path="/task_1_5" element={<Task_1_5 />} />
+                    <Route path="/task_1_6" element={<Task_1_6 />} />
+                </Routes>
+            </Container>
         </Router>
     );
 };
