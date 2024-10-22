9eaf0b91230c740ea6774c38c9c3987319b8aeb7
a.shevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2024-10-22 23:34:37 +0300

    KMBO-NM. Front. T1

--- frontend/src/components/tasks/lab_1_3/components/GraphComponent.tsx
+++ frontend/src/components/tasks/lab_1_3/components/GraphComponent.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import * as Plotly from "plotly.js";
+import {LayoutAxis} from "plotly.js";
 
 const GraphComponent: React.FC = () => {
-    const xValues = Array.from({ length: 100 }, (_, i) => i / 10 - 5); // Генерируем значения x от -5 до 5
+    const xValues = Array.from({ length: 100 }, (_, i) => i / 10 - 10); // Генерируем значения x от -5 до 5
     const sineValues = xValues.map(x => Math.sin(x));
     const cosineValues = xValues.map(x => Math.cos(x));
-    const parabolaValues = xValues.map(x => x ** 2);
 
     const data: Plotly.Data[] = [
         {
@@ -24,27 +24,29 @@ const GraphComponent: React.FC = () => {
             mode: 'lines',
             name: 'Cosine',
             line: { color: 'red' },
-        },
-        {
-            x: xValues,
-            y: parabolaValues,
-            type: 'scatter',
-            mode: 'lines',
-            name: 'Parabola',
-            line: { color: 'green' },
-        },
+        }
     ];
 
-    const layout = {
+    const layout: Partial<Plotly.Layout> = {
         title: 'Mathematical Functions',
-        xaxis: { title: 'X-axis' },
-        yaxis: { title: 'Y-axis' },
+        xaxis: {
+            title: 'X-axis',
+            nticks: 10,
+        },
+        yaxis: {
+            title: 'Y-axis',
+            scaleanchor: 'x', // Привязываем масштаб оси y к оси x
+            nticks: 10,
+        },
         showlegend: true,
+        width: 500, // Устанавливаем ширину графика
+        height: 500, // Устанавливаем высоту графика
     };
 
     return (
         <Plot
             data={data}
+            // @ts-ignore
             layout={layout}
             config={{
                 displaylogo: false,
